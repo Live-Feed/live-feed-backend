87c1cc74ec8cdefa82d88e9da04f85bdd7148199
Jin-Mook <jink0608@naver.com>
2023-12-13 13:00:41 +0900

    [LF-35/batch-modify] batch 프로젝트 세팅

---
+++ LiveFeedBatch/src/test/java/com/livefeed/livefeedbatch/LiveFeedBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livefeed.livefeedbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveFeedBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
