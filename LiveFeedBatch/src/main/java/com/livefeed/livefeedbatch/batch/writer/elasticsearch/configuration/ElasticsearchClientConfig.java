97666172add0c502820f03da2ab27516aa001cd7
Jin-Mook <jink0608@naver.com>
2023-12-23 21:59:59 +0900

    [LF-36/batch-cd] batch 모듈 배포 테스트

--- LiveFeedBatch/src/main/java/com/livefeed/livefeedbatch/batch/writer/elasticsearch/configuration/ElasticsearchClientConfig.java
+++ LiveFeedBatch/src/main/java/com/livefeed/livefeedbatch/batch/writer/elasticsearch/configuration/ElasticsearchClientConfig.java
@@ -1,22 +1,51 @@
 package com.livefeed.livefeedbatch.batch.writer.elasticsearch.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import java.util.List;
+
 @Configuration
 @EnableElasticsearchRepositories(basePackages = {"com.livefeed.livefeedbatch.batch.writer.elasticsearch"})
+@RequiredArgsConstructor
 public class ElasticsearchClientConfig extends ElasticsearchConfiguration {
 
     @Value("${elasticsearch.config.host-and-port}")
     private String hostAndPort;
 
+    @Value("${elasticsearch.config.user}")
+    private String user;
+
+    @Value("{elasticsearch.config.password}")
+    private String password;
+
+    private final Environment environment;
+
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(hostAndPort)
-                .build();
+        return createConfig();
+    }
+
+    private ClientConfiguration createConfig() {
+
+        String[] activeProfiles = environment.getActiveProfiles();
+
+        ClientConfiguration.MaybeSecureClientConfigurationBuilder builder = ClientConfiguration.builder()
+                .connectedTo(hostAndPort);
+
+        if (hasProdProfile(activeProfiles)) {
+            builder.withBasicAuth(user, password);
+        }
+
+        return builder.build();
+    }
+
+    private boolean hasProdProfile(String[] activeProfiles) {
+        return List.of(activeProfiles).contains("prod");
     }
 }
