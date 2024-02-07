65e765337ead31025b6298c85afba7d1f38c3159
Jin-Mook <jink0608@naver.com>
2024-02-07 14:21:02 +0900

    [LF-47/app-monitor] prometheus pushgateway 적용 진행 중
    - service 모듈 적용 완료
    - batch 모듈에 pushgateway 적용 필요

---
+++ LiveFeedBatch/src/main/java/com/livefeed/livefeedbatch/config/PushgatewayConfiguration.java
@@ -0,0 +1,56 @@
+package com.livefeed.livefeedbatch.config;
+
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exporter.PushGateway;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class PushgatewayConfiguration {
+
+    @Value("${prometheus.job.name}")
+    private String prometheusJobName;
+
+    @Value("${prometheus.grouping.key}")
+    private String prometheusGroupingKey;
+
+    @Value("${prometheus.pushgateway.url}")
+    private String prometheusPushGatewayUrl;
+
+    private Map<String, String> groupingKey = new HashMap<>();
+
+    private PushGateway pushGateway;
+
+    private CollectorRegistry collectorRegistry;
+
+    @PostConstruct
+    public void init() {
+        log.info("pushgateway post construct");
+        pushGateway = new PushGateway(prometheusPushGatewayUrl);
+        groupingKey.put(prometheusGroupingKey, prometheusJobName);
+        PrometheusMeterRegistry prometheusMeterRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        collectorRegistry = prometheusMeterRegistry.getPrometheusRegistry();
+        Metrics.globalRegistry.add(prometheusMeterRegistry);
+    }
+
+    @Scheduled(fixedRateString = "5000")
+    public void pushMetrics() {
+        try {
+            pushGateway.pushAdd(collectorRegistry, prometheusJobName, groupingKey);
+            log.info("push test");
+        }
+        catch (Throwable ex) {
+            log.error("Unable to push metrics to Prometheus Push Gateway", ex);
+        }
+    }
+}
