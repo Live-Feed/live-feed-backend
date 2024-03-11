d157c874f5f09692947ea05f623b25ccd9116ebb
jinmook jeong <91299082+jin-mook@users.noreply.github.com>
2024-03-11 09:11:14 +0900

    [LF-52/deploy] api service 어플리케이션 배포 (#40)
    * [LF-52/deploy] service module 배포 작업
    
    - actuator 포트 변경
    
    * [LF-52/deploy] allow-origin 환경변수로 변경
    
    * [LF-52/deploy] github actions 파일 수정
    
    * [LF-52/deploy] oracledb 사용할 수 있도록 docker-compose 파일 수정

--- LiveFeedService/src/main/java/com/livefeed/livefeedservice/common/configuration/WebConfiguration.java
+++ LiveFeedService/src/main/java/com/livefeed/livefeedservice/common/configuration/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.livefeed.livefeedservice.common.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration {
 
+    @Value("${application.allow-origin}")
+    private String origin;
+
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins(origin);
             }
         };
     }
