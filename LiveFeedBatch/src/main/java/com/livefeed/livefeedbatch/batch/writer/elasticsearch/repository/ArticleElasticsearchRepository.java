bb72756e47f81058919f4c25d96a6fe8e757eba4
Jin-Mook <jink0608@naver.com>
2024-06-09 21:15:41 +0900

    [LF-77/delete-article] elasticsearch 날짜별 인덱싱으로 변경

--- LiveFeedBatch/src/main/java/com/livefeed/livefeedbatch/batch/writer/elasticsearch/repository/ArticleElasticsearchRepository.java
+++ LiveFeedBatch/src/main/java/com/livefeed/livefeedbatch/batch/writer/elasticsearch/repository/ArticleElasticsearchRepository.java
@@ -1,7 +1,10 @@
 package com.livefeed.livefeedbatch.batch.writer.elasticsearch.repository;
 
 import com.livefeed.livefeedbatch.batch.writer.elasticsearch.entity.ElasticsearchArticle;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface ArticleElasticsearchRepository extends ElasticsearchRepository<ElasticsearchArticle, Long> {
+import java.util.List;
+
+public interface ArticleElasticsearchRepository {
+
+    void saveAll(List<ElasticsearchArticle> articles);
 }
